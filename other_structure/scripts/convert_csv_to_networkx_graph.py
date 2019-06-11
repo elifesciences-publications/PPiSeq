495f018cca9d40a61b63cb854c9c5677b414399b
darachm <nope>
2019-06-11 14:44:52 -0700

    changed to networkx both the types of csv datas, raw and averaged

--- other_structure/scripts/convert_csv_to_networkx_graph.py
+++ other_structure/scripts/convert_csv_to_networkx_graph.py
@@ -2,6 +2,8 @@
 
 # This expects this schema:
 # PPI,Number_of_Barcodes,Barcode_sequences,Fitness,Fitness_estimaion_error,Counts_G0,Counts_G6,Counts_G12,Counts_G18
+# or 
+# PPI,Number_of_barcodes,Mean_fitness,SD,P_value,FDR_adjusted_P_value,Positive
 
 import argparse
 import csv
@@ -17,15 +19,30 @@ parser.add_argument("input_csv",type=str,
 parser.add_argument("output_file",type=str,
     help="output path"
     )
+parser.add_argument("--type",type=str,
+    help="raw or mean?"
+    )
 args = parser.parse_args()
 
 g = networkx.MultiDiGraph()
 
-with open(args.input_csv,'r') as f:
-    c = csv.reader(f,delimiter=',')
-    next(c)
-    for i in c:
-        idz = i[0].split("_")
-        g.add_edge(idz[0],idz[1],fitness=i[3],error=i[4])
+if args.type == "raw":
+    with open(args.input_csv,'r') as f:
+        c = csv.reader(f,delimiter=',')
+        next(c)
+        for i in c:
+            idz = i[0].split("_")
+            g.add_edge(idz[0],idz[1],fitness=i[3],error=i[4])
+elif args.type == "mean":
+    with open(args.input_csv,'r') as f:
+        c = csv.reader(f,delimiter=',')
+        next(c)
+        for i in c:
+            if i[6] == 0:
+                continue
+            idz = i[0].split("_")
+            g.add_edge(idz[0],idz[1],fitness=i[2],error=i[3],fdr=i[5])
+else:
+    raise()
 
 networkx.write_gpickle(g,args.output_file)
