6d3d0e25e4dc87135945b7b28b6202fde373f591
Sasha Levy <sasha@vpn-clients-gw2-113-111.slac.stanford.edu>
2019-06-17 09:13:12 -0700

    Update make_PPI_correlation_matrix.R

--- working_code/Sasha/make_PPI_correlation_matrix.R
+++ working_code/Sasha/make_PPI_correlation_matrix.R
@@ -37,3 +37,78 @@ for(i in 1:nrow(ppi)){
 fitness_by_env_list = l
 save(fitness_by_env_list, file = "~/Dropbox/PPiSeq_02/Working_data/fitness_by_env_list.Rfile")
 
+#make a big matrix for bait
+x = NULL
+for(i in 1:length(fitness_by_env_list)){
+  x = cbind(x, fitness_by_env_list[[i]])
+}
+n = NULL
+for(i in 1:length(fitness_by_env_list)){
+  n = c(n, paste(names(fitness_by_env_list)[i], colnames(fitness_by_env_list[[i]]), sep = '_'))
+}
+colnames(x) = n
+rownames(x) = rownames(fitness_by_env_list[[1]])
+bait_fitness_all_env = x
+save(bait_fitness_all_env , file = "~/Dropbox/PPiSeq_02/Working_data/bait_fitness_all_env.Rfile")
+
+#make a big matrix for prey
+x = NULL
+for(i in 1:length(fitness_by_env_list)){
+  x = rbind(x, fitness_by_env_list[[i]])
+}
+n = NULL
+for(i in 1:length(fitness_by_env_list)){
+  n = c(n, paste(names(fitness_by_env_list)[i], rownames(fitness_by_env_list[[i]]), sep = '_'))
+}
+rownames(x) = n
+colnames(x) = colnames(fitness_by_env_list[[1]])
+prey_fitness_all_env = x
+save(prey_fitness_all_env , file = "~/Dropbox/PPiSeq_02/Working_data/prey_fitness_all_env.Rfile")
+
+#make bait correlation matrix
+x = matrix(NA, nrow(bait_fitness_all_env), nrow(bait_fitness_all_env))
+y = rownames(bait_fitness_all_env)
+rownames(x) = y
+colnames(x) = y
+for(i in 1:nrow(bait_fitness_all_env)){
+  for(j in 1:nrow(bait_fitness_all_env)){
+    x[i,j] = cor(bait_fitness_all_env[y[i],], bait_fitness_all_env[y[j],], use = "na.or.complete")
+  }
+}
+#remove empty rows, columns
+r = names(which(apply(x, 1, sum, na.rm = T) == 0))
+x = x[which(!rownames(x) %in% r), which(!colnames(x) %in% r)]
+#save
+bait_cor_matrix = x
+save(bait_cor_matrix, file = "~/Dropbox/PPiSeq_02/Working_data/bait_cor_matrix.Rfile")
+
+#make prey correlation matrix
+load("~/Dropbox/PPiSeq_02/Working_data/prey_fitness_all_env.Rfile")
+x = matrix(NA, ncol(prey_fitness_all_env), ncol(prey_fitness_all_env))
+y = colnames(prey_fitness_all_env)
+rownames(x) = y
+colnames(x) = y
+for(i in 1:ncol(prey_fitness_all_env)){
+  for(j in 1:ncol(prey_fitness_all_env)){
+    x[i,j] = cor(prey_fitness_all_env[,y[i]], prey_fitness_all_env[,y[j]], use = "na.or.complete")
+  }
+}
+r = names(which(apply(x, 1, sum, na.rm = T) == 0))
+x = x[which(!rownames(x) %in% r), which(!colnames(x) %in% r)]
+prey_cor_matrix = x
+save(prey_cor_matrix, file = "~/Dropbox/PPiSeq_02/Working_data/prey_cor_matrix.Rfile")
+
+
+#visualize
+load("~/Dropbox/PPiSeq_02/Working_data/bait_cor_matrix.Rfile")
+require(ggplot2)
+require(gplots)
+x = bait_cor_matrix
+y = as.data.frame(x)
+p = 
+
+pdf(file = "~/Desktop/test.pdf" )
+heatmap.2(x, na.color = "black", dendrogram = "col", labRow = F, labCol = F, trace = "none")
+dev.off()
+
+
