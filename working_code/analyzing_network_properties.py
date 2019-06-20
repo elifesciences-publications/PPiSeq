ced04442fae6027164b3b65708d871fa36053e2b
darachm <nope>
2019-06-19 21:28:07 -0700

    Fixing incompatibility of script with a multigraph

--- working_code/analyzing_network_properties.py
+++ working_code/analyzing_network_properties.py
@@ -55,38 +55,42 @@ if args.figure_base:
 hairball = plt.figure(figsize=(32, 32))
 pos = nx.layout.spring_layout(this_net)
 node_sizes = np.sqrt(de)
-line_widths = [ float(this_net.get_edge_data(u,v)['fitness']) for u,v in this_net.edges() ]
 nodes = nx.draw_networkx_nodes(this_net, pos, node_size=node_sizes, node_color='blue')
-edges = nx.draw_networkx_edges(this_net, pos, node_size=node_sizes, width=line_widths, alpha=0.5)
+try:
+    line_widths = [ float(this_net.get_edge_data(u,v)['fitness']) for u,v in this_net.edges() ]
+    edges = nx.draw_networkx_edges(this_net, pos, node_size=node_sizes, width=line_widths, alpha=0.5)
+except:
+    edges = nx.draw_networkx_edges(this_net, pos, node_size=node_sizes, width=0.2, alpha=0.5)
 plt.title(net_name+": net of all connections")
 if args.figure_base:
     hairball.savefig(args.figure_base+net_name+"_hairball.svg")
 
-ecorig = nx.eigenvector_centrality(this_net)
-ec = list(ecorig.values())
-
-csvprinter(ecorig,("YORF","eigenvector_centrality"),"_node_eigenvectorcentrality.csv")
-
-pos = nx.layout.spring_layout(this_net)
-eigenvector_centrality_hist = plt.figure()
-plt.hist(ec)
-#plt.semilogx()
-plt.title("Barplot of node eigenvector centrality")
-plt.ylabel("Count of nodes")
-plt.xlabel("Eigenvector centrality")
-if args.figure_base:
-    eigenvector_centrality_hist.savefig(args.figure_base+net_name+"_eigencenter_histogram.svg")
-
-
-pos = nx.layout.spring_layout(this_net)
-eigencenter_hairball = plt.figure(figsize=(16,8))
-nodes = nx.draw_networkx_nodes(this_net, pos, node_size=5, node_color=ec)
-edges = nx.draw_networkx_edges(this_net, pos, width=0.1, alpha=0.2)
-nodes.set_norm(matplotlib.colors.Normalize())
-plt.colorbar(nodes)
-plt.legend()
-axes = plt.gca()
-axes.set_xlim([-.2,.2])
-axes.set_ylim([-.2,.2])
-if args.figure_base:
-    eigencenter_hairball.savefig(args.figure_base+net_name+"_eigencenter_hairball.svg")
+if type(this_net) == type(nx.Graph()):
+    ecorig = nx.eigenvector_centrality(this_net)
+    ec = list(ecorig.values())
+    
+    csvprinter(ecorig,("YORF","eigenvector_centrality"),"_node_eigenvectorcentrality.csv")
+    
+    pos = nx.layout.spring_layout(this_net)
+    eigenvector_centrality_hist = plt.figure()
+    plt.hist(ec)
+    #plt.semilogx()
+    plt.title("Barplot of node eigenvector centrality")
+    plt.ylabel("Count of nodes")
+    plt.xlabel("Eigenvector centrality")
+    if args.figure_base:
+        eigenvector_centrality_hist.savefig(args.figure_base+net_name+"_eigencenter_histogram.svg")
+    
+    
+    pos = nx.layout.spring_layout(this_net)
+    eigencenter_hairball = plt.figure(figsize=(16,8))
+    nodes = nx.draw_networkx_nodes(this_net, pos, node_size=5, node_color=ec)
+    edges = nx.draw_networkx_edges(this_net, pos, width=0.1, alpha=0.2)
+    nodes.set_norm(matplotlib.colors.Normalize())
+    plt.colorbar(nodes)
+    plt.legend()
+    axes = plt.gca()
+    axes.set_xlim([-.2,.2])
+    axes.set_ylim([-.2,.2])
+    if args.figure_base:
+        eigencenter_hairball.savefig(args.figure_base+net_name+"_eigencenter_hairball.svg")
