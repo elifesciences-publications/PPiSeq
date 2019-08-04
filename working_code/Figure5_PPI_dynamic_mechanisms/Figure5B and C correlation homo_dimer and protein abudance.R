767d52b0abb8042d91d8e5c1d8ec59a38862fcf5
Zhimin <zmliu354@gmail.com>
2019-08-04 10:39:01 -0700

    Polish Figure 5

--- working_code/Figure5_PPI_dynamic_mechanisms/Figure5B and C correlation homo_dimer and protein abudance.R
+++ working_code/Figure5_PPI_dynamic_mechanisms/Figure5B and C correlation homo_dimer and protein abudance.R
@@ -92,19 +92,25 @@ colnames(homo_dimer_matrix_final) = c("Protein", "Homo-dimer", "Correlation",
                                       "Protein_amount", "Homo_amount", "Difference_amount")
 csvWriter(homo_dimer_matrix_final, "Working_data/homo_dimer/PPI_homo_dimer_correlation_abundance.csv")
 
+#############
+setwd("~/Dropbox/PPiSeq_02/")
 homo_plot_matrix = dataFrameReader_T("Working_data/homo_dimer/PPI_homo_dimer_correlation_abundance.csv")
 
 self_interacting = dataFrameReader_T("Working_data/homo_dimer/Self_protein_matrix.csv")
 self_interacting_large = as.character(self_interacting[which(self_interacting$PPI_number >5 
-                                                             & abs(self_interacting$Mean_cor) >= 0.4
+                                                             & abs(self_interacting$Mean_cor) >= 0.5
                                                           ),1])
+
 #self_interacting_large = as.character(self_interacting[which(abs(self_interacting$Mean_cor) >= 0.2 
 #& self_interacting$PPI_number >=20),1])
 homo_plot_matrix_large = homo_plot_matrix[which(as.character(homo_plot_matrix[,2]) %in% self_interacting_large),] #2488
-length(which(homo_plot_matrix_large$Correlation > 0 & homo_plot_matrix_large$Difference_amount > 0)) #1379
-length(which(homo_plot_matrix_large$Correlation > 0 & homo_plot_matrix_large$Difference_amount < 0)) #812
-length(which(homo_plot_matrix_large$Correlation < 0 & homo_plot_matrix_large$Difference_amount < 0)) #119
-length(which(homo_plot_matrix_large$Correlation < 0 & homo_plot_matrix_large$Difference_amount > 0)) #136
+
+homo_plot_matrix_chosen = homo_plot_matrix[which(as.character(homo_plot_matrix[,2]) == "YML017W"),]
+
+length(which(homo_plot_matrix_large$Correlation > 0 & homo_plot_matrix_large$Difference_amount > 0)) #790
+length(which(homo_plot_matrix_large$Correlation > 0 & homo_plot_matrix_large$Difference_amount < 0)) #350
+length(which(homo_plot_matrix_large$Correlation < 0 & homo_plot_matrix_large$Difference_amount < 0)) #37
+length(which(homo_plot_matrix_large$Correlation < 0 & homo_plot_matrix_large$Difference_amount > 0)) #62
 diff = homo_plot_matrix$Difference_amount # min: -10.45546; max: 5.863211
 CC = homo_plot_matrix$Correlation # -0.9259513; max:0.9903858
 library(scales)
@@ -135,7 +141,30 @@ ggplot() +
         legend.key=element_blank()) +
   theme(axis.text.x = element_text(size = 10, color = "black"),
         axis.text.y.left = element_text(size = 10, color = "black"))
-ggsave("~/Desktop/Figure5B_abudance_change_versus_correlation_homo_dimer_>5_PPI_>0.4_dot.pdf", width = 7, height =5)
+ggsave("~/Desktop/Cor_abundance/Figure5B_abudance_change_versus_correlation_homo_dimer_>5_PPI_>0.5_dot.pdf", width = 6, height =5)
+
+ggplot() +
+  #geom_hex(aes(x = Difference_amount, y = Correlation), homo_plot_matrix_large, size = 0.05, bins = 60)+
+  #scale_fill_gradientn(colours = c(apple_colors[5], apple_colors[10], apple_colors[7]))+
+  geom_point(aes(x = Difference_amount, y = Correlation), homo_plot_matrix_chosen, alpha = 0.5, col = apple_colors[5])+
+  geom_vline(xintercept = 0, col = apple_colors[7])+
+  geom_hline(yintercept = 0, col = apple_colors[7])+
+  scale_y_continuous(name = "Correlation coefficient with homo-dimer",
+                     limits=c(-1, 1),
+                     breaks= seq(-1,1, by = 0.5),
+                     labels =seq(-1,1, by = 0.5)) +
+  
+  scale_x_continuous(name = "Difference of protein abundance with homo-dmer", 
+                     limits=c(-6, 6),
+                     breaks=seq(-6, 6, by =2),
+                     labels = seq(-6, 6, by= 2)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.key=element_blank()) +
+  theme(axis.text.x = element_text(size = 10, color = "black"),
+        axis.text.y.left = element_text(size = 10, color = "black"))
+ggsave("~/Desktop/Cor_abundance/abudance_change_versus_correlation_homo_dimer_YML017W_dot.pdf", width = 5, height =5)
+
 
 ########## Correlation between homo-dimer correaltion with ratio of larger proteins
 homo_plot_matrix = csvReader_T("Working_data/homo_dimer/PPI_homo_dimer_correlation_abundance.csv")
