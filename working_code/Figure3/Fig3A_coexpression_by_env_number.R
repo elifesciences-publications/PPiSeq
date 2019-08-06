5a08ed402566571aecd48392b7bff77099416163
Sasha Levy <sasha@DN52es2t.SUNet>
2019-08-06 09:19:16 -0700

    Figure 3 updates

--- working_code/Figure3/Fig3A_coexpression_by_env_number.R
+++ working_code/Figure3/Fig3A_coexpression_by_env_number.R
@@ -30,7 +30,7 @@ df$env.number = as.factor(df$env.number)
 
 pdf(file = "~/Dropbox/PPiSeq_02/Working_figure/Figure3/Figure3A_coexpression_by_env_number.pdf", height = 6, width = 4)
 p <- ggplot(df, aes(x=env.number, y=ppi_coex, fill=env.number)) +   
-  geom_boxplot(notch = T) +  labs(x="Environments in which a PPI is observed", y = "Co-expression mutual mank")
+  geom_boxplot(notch = T) +  labs(x="Environments in which a PPI is observed", y = "Co-expression mutual rank")
 p + theme_classic() + theme(axis.text.y = element_text(  size=10, angle=45), 
          axis.text.x = element_text(size=10,), 
          panel.background = element_rect(fill = "white"), legend.position="none") + scale_fill_manual(values = c("#4575b4","#74add1","#abd9e9","#e0f3f8","#ffffbf","#fee090", "#fdae61","#f46d43","#d73027")) 
--- working_code/Figure3/Fig3C_gene_features.R
+++ working_code/Figure3/Fig3C_gene_features.R
@@ -1,7 +1,6 @@
 setwd("~/Dropbox/PPiSeq_02/")
 
-apple_colors = c("#5AC8FA", "#FFCC00", "#FF9500", "#FF2D55", "#007AFF", "#4CD964", "#FF3B30",
-                 "#8E8E93", "#EFEFF4", "#CECED2", "#000000", "007AFF")
+paper.colors = c("#4575b4","#74add1","#abd9e9","#e0f3f8","#ffffbf","#fee090", "#fdae61","#f46d43","#d73027")
 
 #Make a combined data matrix with variation score
 load("Working_data/Positive_PPI_environment/variation_score.Rfile")
@@ -15,10 +14,10 @@ colnames(gene_feature_matched)[41] = "vScore"
 features = c(colnames(gene_feature), "GIdegree", "vScore")
 features_chosen = features[c(18, 19, 12, 7, 25, 15, 11, 20, 14, 16, 3, 2, 8, 21, 23, 4, 29, 40 )]
 feature_names = c("dN/dS", "Protein disorder", "Number of complexes" ,  "Protein length", "Phenotypic capacitance", 
-                  "PPI degree, Y2H" , "Curated phenotypes", "Single mutant fitness defect", 'PPI degree, Tap MS', 
+                  "PPI degree, Y2H" , "Curated phenotypes", "Deletion fitness defect", 'PPI degree, Tap MS', 
                   "Number of domains", "Multifunctionality", "Yeast conservation", "Broad conservation", 
-                  "Codon adaptation index",  "Expression level", "PPI degree, Biogrid", "Protein abundance", "GI degree", "vScore" )
-degree_bins = matrix(c(0, 4, 5, 14, 15, 1000), 2, 3)
+                  "Codon adaptation index",  "Expression level", "PPI degree, Biogrid", "Protein abundance", "Genetic interaction degree" )
+degree_bins = matrix(c(0, 4, 5, 14, 15, 1000, 0, 1000), 2, 4)
 n = 1:ncol(degree_bins)
 for(i in 1:ncol(degree_bins)){
   n[i]=paste(degree_bins[1,i], degree_bins[2,i], sep = "-")
@@ -29,7 +28,7 @@ degree_bin_names = n
 vScore = as.numeric(gene_feature_matched[,41])
 degree = as.numeric(gene_feature_matched[,4])
 f_cor = matrix(0, length(features_chosen), ncol(degree_bins))
-colnames(f_cor) = n
+colnames(f_cor) =degree_bin_names
 rownames(f_cor) = feature_names
 for(j in 1:ncol(degree_bins)){
   n = length(which(degree >= degree_bins[1,j] & degree <= degree_bins[2,j]))
@@ -52,14 +51,14 @@ for(k in 1:1000){
   degree_b = as.numeric(g_b[,4])
   f_cor_b = matrix(0, length(features_chosen), ncol(degree_bins))
   colnames(f_cor_b) = degree_bin_names
-  rownames(f_cor_b) = features_chosen
+  rownames(f_cor_b) = feature_names
   for(j in 1:ncol(degree_bins)){
     #n = length(which(degree_b >= degree_bins[1,j] & degree_b <= degree_bins[2,j]))
     #delta = 1.96/sqrt(n - 3)
     #print(length(which(degree > degree_bins[1,j] & degree < degree_bins[2,j])))
     for(i in 1:length(features_chosen)){
       x = as.numeric(g_b[which(degree_b > degree_bins[1,j] & degree_b < degree_bins[2,j]), features_chosen[i]])
-      y = vScore[which(degree_b > degree_bins[1,j] & degree_b < degree_bins[2,j])]
+      y = vScore_b[which(degree_b > degree_bins[1,j] & degree_b < degree_bins[2,j])]
       f_cor_b[i,j] = cor(x,y, method = 'spearman', use = 'complete.obs' )
     }
   }
@@ -67,15 +66,46 @@ for(k in 1:1000){
 }
 
 #make a mena and SD matrix from bootstrapped data
-sd = f_cor_boot[[1]]
-mean = f_cor_boot[[1]]
-sem = f_cor_boot[[1]]
-for(i in 1:nrow(sd)){
-  for(j in 1:ncol(sd)){
+b.sd = f_cor_boot[[1]]
+b.mean = f_cor_boot[[1]]
+b.sem = f_cor_boot[[1]]
+for(i in 1:nrow(b.sd)){
+  for(j in 1:ncol(b.sd)){
     x = sapply(f_cor_boot, function(a) a[i,j])
-    sd[i,j] = sd(x)
-    mean[i,j] = mean(x)
-    sem[i,j] = sd(x)/sqrt(1000)
+    b.sd[i,j] = sd(x)
+    b.mean[i,j] = mean(x)
+    b.sem[i,j] = sd(x)/sqrt(1000)
   }
 }
+
+
+#The supplemenatal plot using all data
+i = ncol(b.mean)
+o = order(b.mean[,i], decreasing = F)
+
+pdf(file = "Working_figure/SFigures/FigureSX_related_to_Fig3C.pdf")
+par(mar= c(5,10,1,1))
+barCenter = barplot(b.mean[o, i], horiz=T, beside=F, xlim=c(-0.5, 0.3), 
+                    xlab="Spearman correlation with protein interaction dynamicity",
+                    axisnames=F, border=NA, cex.axis=1, col = paper.colors[2])
+arrows( b.mean[o, i] - b.sd[o,i], barCenter,  b.mean[o, i] + b.sd[o,i], barCenter, angle = 90, lwd = 1.5, code = 3, length = 0.05)
+text(-0.65, barCenter, labels= rownames(f_cor)[o], cex = 0.8, xpd = TRUE)
+dev.off()
+
+#Comparison by degree of some major features
+fs = c(17, 15, 18, 8,  3,  2, 1)
+fc = b.mean[fs, 3:1]
+fc_ci_lower = b.mean[fs, 3:1]- b.sd[fs, 3:1] 
+fc_ci_upper = b.mean[fs, 3:1]+ b.sd[fs, 3:1] 
+pdf(file = "Working_figure/Figure3/Figure3C_gene_features_PPI_variability.pdf")
+par(mar= c(5,8,1,1))
+barCenter = barplot(t(fc), horiz=T, beside=T, xlim=c(-0.6, 0.4), 
+                    xlab="Correlation with PPI variability score",
+                    axisnames=F, border=NA, cex.axis=1, col = paper.colors[c(2,6,8)])
+arrows( t(fc_ci_lower), barCenter, t(fc_ci_upper), barCenter, angle = 90, lwd = 1.5, code = 3, length = 0.05)
+text(-0.65, barCenter[2,], labels= rownames(fc), cex = 1, xpd = TRUE)
+legend(.2, 5, c("0-4", "5-14", "15+"), bty = "n", title = "PPI degree", 
+       fill = paper.colors[c(8,6,2)])
+dev.off()
+
 
