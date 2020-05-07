29c20017db638a79440a82ea2d0fcad881743100
Zhimin <zmliu354@gmail.com>
2020-05-07 12:18:57 -0700

    remove_old_codes

--- working_code/make_goslim_id_file.R
+++
@@ -1,6 +0,0 @@
-
-go = as.matrix(read.table("~/Dropbox/PPiSeq_02/Working_data/go_slim_mapping_tab_20190405.txt",sep = "\t", header = F)) 
-c = which(go[,4] == "C")
-cc = go[c,5]
-names(cc) = go[c,1]
-save(cc, file="~/Dropbox/PPiSeq_02/Working_data/GOSlim_CC.Rfile" )
